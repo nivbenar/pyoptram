ed5051c80dca7fc1e2b858bda64a4e94580396ad
Niv <nivbenar@post.bgu.ac.il>
2026-04-21 13:13:44 +0300

    Make a package called pyOPTRAM

---
+++ pyoptram/__init__.py
@@ -0,0 +1,8 @@
+from .acquire import acquire_optram_inputs
+from .str_transform_calculations import calculate_str, optram_calculate_str
+
+__all__ = [
+    "acquire_optram_inputs",
+    "calculate_str",
+    "optram_calculate_str",
+]
