95dc6f1d412011597f5436b4732aa7b495ee1f08
nivbenar <nivbenar@post.bgu.ac.il>
2026-04-23 09:36:53 +0300

    docstring __init__

--- pyoptram/__init__.py
+++ pyoptram/__init__.py
@@ -1,6 +1,9 @@
 from .acquire import acquire_optram_inputs
 from .str_transform_calculations import calculate_str, optram_calculate_str
 
+## pyOPTRAM Package
+##Public API from this package
+
 __all__ = [
     "acquire_optram_inputs",
     "calculate_str",
