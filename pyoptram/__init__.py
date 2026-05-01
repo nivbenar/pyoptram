36fe04bdd71ada6c6600209345a866b05878078d
nivbenar <nivbenar@post.bgu.ac.il>
2026-05-01 19:52:36 +0300

    Update __init__.py

--- pyoptram/__init__.py
+++ pyoptram/__init__.py
@@ -1,9 +1,10 @@
 from .acquire import acquire_optram_inputs, get_cdse_token
 from .ndvi_str import optram_ndvi_str
 from .str_transform_calculations import calculate_str, optram_calculate_str
+from .trapezoid import optram_wetdry_coefficients, plot_vi_str_cloud
 
 ## pyOPTRAM Package
-##Public API from this package
+## Public API from this package
 
 __all__ = [
     "acquire_optram_inputs",
@@ -11,4 +12,6 @@ __all__ = [
     "optram_ndvi_str",
     "calculate_str",
     "optram_calculate_str",
+    "optram_wetdry_coefficients",
+    "plot_vi_str_cloud",
 ]
