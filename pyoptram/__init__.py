159dd225dec79e4d5bc077e5944e5d276da37a89
Niv <nivbenar@post.bgu.ac.il>
2026-05-02 15:46:58 +0300

    Add SCL masked NDVI acquisition

--- pyoptram/__init__.py
+++ pyoptram/__init__.py
@@ -1,5 +1,6 @@
 from .acquire import acquire_optram_inputs, get_cdse_token
 from .ndvi_str import optram_ndvi_str
+from .soil_moisture import calculate_soil_moisture, optram_calculate_soil_moisture
 from .str_transform_calculations import calculate_str, optram_calculate_str
 from .trapezoid import optram_wetdry_coefficients, plot_vi_str_cloud
 
@@ -10,6 +11,8 @@ __all__ = [
     "acquire_optram_inputs",
     "get_cdse_token",
     "optram_ndvi_str",
+    "calculate_soil_moisture",
+    "optram_calculate_soil_moisture",
     "calculate_str",
     "optram_calculate_str",
     "optram_wetdry_coefficients",
